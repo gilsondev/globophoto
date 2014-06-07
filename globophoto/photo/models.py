79bd7354f9a450b38c98898dba74a54c08a2715a
gilsondev <contato@gilsondev.com>
2014-06-07 14:08:43 -0300

    Insert name field

--- globophoto/photo/models.py
+++ globophoto/photo/models.py
@@ -4,4 +4,5 @@ from django.db import models
 
 
 class Photo(models.Model):
+    name = models.CharField(max_length=255)
     image = models.FileField(upload_to="images", max_length=255)
--- globophoto/photo/tests/test_models.py
+++ globophoto/photo/tests/test_models.py
@@ -6,7 +6,14 @@ from globophoto.photo.models import Photo
 
 
 class PhotoTest(TestCase):
+    def setUp(self):
+        self.fields = Photo._meta.get_all_field_names()
+
     def test_should_file_field(self):
         """Should have file field in model"""
-        fields = Photo._meta.get_all_field_names()
-        self.assertIn("image", fields)
+        self.assertIn("image", self.fields)
+
+    def test_should_name_field(self):
+        """Should have name field in model"""
+        self.assertIn("name", self.fields)
+
